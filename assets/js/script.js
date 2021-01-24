b8209d64f3b0edea70e2b592f2d324668ff1e5b9
symlar <40181569+symlara@users.noreply.github.com>
2021-01-23 21:45:38 -0600

    added textarea elements to index file for localStorage ids to connect to. Added more code to the .btn-save in the js file for localStorage purposes also.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,22 @@
-var task= {};
+var task = [];
 
+var date = moment().format("dddd, MMMM D");
+console.log(date);
 
 // elements that make up an item
 var ItemEl = $("<div>").addClass("group-item");
 
-var updateItem= function(taskEl) {
-    var time = moment().format("ddd, hA");
 
-};
+
+
 
 // local storage call to save whatever is in the class called 'task'
 
-let saveItems = 'task';
-localStorage.setItem(saveItems, 'task');
-
-
-
-
-    if (!task) {
-        tasks = {
-            item1: [],
-            item2: [],
-            item3: [],
-            item4: [],
-            item5: [],
-            item6: [],
-            item7: [],
-            item8: [],
-            item9: []
-        };
-    }
-
-    $.each(task, function(list, arr) {
-        console.log(list, arr);
-
-        arr.forEach(function(item) {
-            updateItem(item.text, item.time, list);
-        });
-    });
 
 
     // localStorage call to get whatever info is in the 'task' class
 
-let myItem = localStorage.getItem(saveItems);
+
 
 
 // jquery that tells the 'task' input section to change the text when it's clicked
@@ -55,9 +29,20 @@ $(".group-item").on("click", "p", function() {
     textInput.trigger("focus");
 });
 
+localStorage.setItem("Input", "#value1");
+
+var savedData = localStorage.getItem("Input");
+
 // event listener call for save buttons
 $(".btn-save").click(function() {
-    
+    var taskData = $("#value1").val();
+    var taskData = $("#value2").val();
+    var taskData = $("#value3").val()
+    var taskData = $("#value4").val();
+    var taskData = $("#value5").val();
+    var taskData = $("#value6").val();
+    var taskData = $("#value7").val();
+    console.log(".btn-save");
 });
 
 
